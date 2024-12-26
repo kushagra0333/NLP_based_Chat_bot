4d3854d1cf906a41fc64c1e3ef384c8fbf59dc1c
arjavjain5203 <arjavjain.jain1942@gmail.com>
2024-12-26 21:32:13 +0530

    update code

--- chatbot.py
+++ chatbot.py
@@ -36,8 +36,7 @@ def chatbot(input_text):
     for intent in data:
         if intent['tag'] == tag:
             return random.choice(intent['responses'])
-
-counter = 0
+    return "Sorry, I didn't understand that. Can you please clarify?"
 
 # Function to load CSS
 def load_css(file_path):
@@ -49,7 +48,6 @@ css_path = pathlib.Path("style.css")
 load_css(css_path)
 
 def main():
-    global counter
     st.image('image.webp', width=500)
     st.title("CUSTOMER SUPPORT")
     # Creating sidebar options
@@ -59,27 +57,23 @@ def main():
     # Home page
     if choice == 'Home':
         st.write('Welcome to the customer support, please enter your message here')
-        # Check if chat_log file exists
         if not os.path.exists('chat_log.csv'):
             with open('chat_log.csv', 'w', newline='', encoding='utf-8') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(['User Inputs', 'ChatBot Response', 'Timestamp'])
-        counter += 1
-        user_input = st.text_input("USER:", key=f'user_input{counter}')
+        
+        user_input = st.text_input("USER:")
 
         if user_input:
-            # Convert user input to string
-            user_input_str = str(user_input)
-            response = chatbot(user_input_str)
-            st.text_area("ChatBot:", value=response, height=120, max_chars=None, key=f"response{counter}")
+            response = chatbot(user_input)
+            st.text_area("ChatBot:", value=response, height=120, max_chars=None)
 
-            # Get the current timestamp
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            # Save the user input and chatbot response to the chat_log.csv file
             with open('chat_log.csv', 'a', newline='', encoding='utf-8') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow([user_input_str, response, timestamp])
+                writer.writerow([user_input, response, timestamp])
+            
             if response.lower() in ['goodbye', 'bye']:
                 st.write("Thank you for chatting with me. Have a good day!")
                 st.stop()
@@ -87,47 +81,51 @@ def main():
     # Conversation history
     elif choice == 'Conversation History':
         st.header("Conversation History")
-        with open('chat_log.csv', 'r', encoding='utf-8') as csvfile:
-            csv_reader = csv.reader(csvfile)
-            next(csv_reader)
-            for row in csv_reader:
-                st.text(f'User: {row[0]}')
-                st.text(f'ChatBot: {row[1]}')
-                st.text(f'Timestamp: {row[2]}')
-                st.markdown("---")
+        if os.path.exists('chat_log.csv'):
+            with open('chat_log.csv', 'r', encoding='utf-8') as csvfile:
+                csv_reader = csv.reader(csvfile)
+                next(csv_reader)
+                for row in csv_reader:
+                    st.text(f'User: {row[0]}')
+                    st.text(f'ChatBot: {row[1]}')
+                    st.text(f'Timestamp: {row[2]}')
+                    st.markdown("---")
+        else:
+            st.write("No conversation history found.")
 
     # About page
+    # About page
     elif choice == "About":
-        st.header("About")
-        st.write("""
-        Welcome to our Intent-Based Chatbot!
+         st.header("About")
+         st.write("""
+    Welcome to our Intent-Based Chatbot!
 
-        This chatbot is designed to provide an interactive and intuitive experience for users by leveraging Natural Language Processing (NLP) and machine learning techniques. Here's a bit more about how it works and what it offers:
+    This chatbot is designed to provide an interactive and intuitive experience for users by leveraging Natural Language Processing (NLP) and machine learning techniques. Here's a bit more about how it works and what it offers:
 
-        ### How It Works
-        Our chatbot utilizes a Logistic Regression model to predict the intent behind the user's input. This model is trained on a dataset of various user queries and responses, allowing it to accurately classify and respond to a wide range of inquiries.
+    ### How It Works
+    Our chatbot utilizes a Logistic Regression model to predict the intent behind the user's input. This model is trained on a dataset of various user queries and responses, allowing it to accurately classify and respond to a wide range of inquiries.
 
-        ### Features
-        - **Intent Recognition**: The chatbot can understand and categorize user inputs into predefined intents, ensuring relevant and helpful responses.
-        - **Dynamic Responses**: Based on the recognized intent, the chatbot generates appropriate responses, making the interaction seamless and engaging.
-        - **User-Friendly Interface**: Implemented using Streamlit, the chatbot provides a clean and intuitive web interface, making it easy for users to interact and get the information they need.
+    ### Features
+    - *Intent Recognition*: The chatbot can understand and categorize user inputs into predefined intents, ensuring relevant and helpful responses.
+    - *Dynamic Responses*: Based on the recognized intent, the chatbot generates appropriate responses, making the interaction seamless and engaging.
+    - *User-Friendly Interface*: Implemented using Streamlit, the chatbot provides a clean and intuitive web interface, making it easy for users to interact and get the information they need.
 
-        ### Technologies Used
-        - **Natural Language Processing (NLP)**: To process and understand the text input from users.
-        - **Logistic Regression**: A machine learning algorithm used to classify the intents.
-        - **Streamlit**: A powerful tool for creating and deploying web applications quickly and efficiently.
+    ### Technologies Used
+    - *Natural Language Processing (NLP)*: To process and understand the text input from users.
+    - *Logistic Regression*: A machine learning algorithm used to classify the intents.
+    - *Streamlit*: A powerful tool for creating and deploying web applications quickly and efficiently.
 
-        ### Why Use This Chatbot?
-        Whether you need quick answers, help with specific tasks, or just want to explore, our chatbot is here to assist you. It is designed to handle various types of queries and provide accurate and timely responses.
+    ### Why Use This Chatbot?
+    Whether you need quick answers, help with specific tasks, or just want to explore, our chatbot is here to assist you. It is designed to handle various types of queries and provide accurate and timely responses.
 
-        ### Future Enhancements
-        We are continually working to improve our chatbot by incorporating more advanced machine learning models, expanding the dataset, and adding new features to enhance user interaction.
+    ### Future Enhancements
+    We are continually working to improve our chatbot by incorporating more advanced machine learning models, expanding the dataset, and adding new features to enhance user interaction.
 
-        ### Contact Us
-        If you have any feedback or questions about our chatbot, feel free to reach out to us. We are always looking to improve and appreciate your input.
+    ### Contact Us
+    If you have any feedback or questions about our chatbot, feel free to reach out to us. We are always looking to improve and appreciate your input.
 
-        Thank you for using our Intent-Based Chatbot. We hope it makes your experience enjoyable and efficient!
-        """)
-        
+    Thank you for using our Intent-Based Chatbot. We hope it makes your experience enjoyable and efficient!
+    """)
+
 if __name__ == '__main__':
     main()
